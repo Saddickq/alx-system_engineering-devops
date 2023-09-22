18d6e9f8826d23220fc73790e8d4606a4fa6a96c
Abubakr Saddickq <abubakrsaddickq@gmail.com>
2023-09-22 08:39:18 -0700

    refine puppet manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Using Puppet, install flask from pip3.
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => 'pip3 install flask==2.1.0',
   path    => '/usr/bin/',
 }
